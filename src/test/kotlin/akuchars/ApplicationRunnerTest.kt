76ccab57a261e6cb5bcd270bf013fd883a8f2d0c
akuchars <adam.kucharski@unity.pl>
2019-08-27 15:06:03 +0200

    Adding ApplicationRunnerTest.kt which check taht spring boot for test profile is started

---
+++ src/test/kotlin/akuchars/ApplicationRunnerTest.kt
@@ -0,0 +1,16 @@
+package akuchars
+
+import akuchars.kernel.ProfileProperties
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles(value = [ProfileProperties.TEST_PROFILE, ProfileProperties.H2_PROFILE])
+internal class ApplicationRunnerTest {
+
+	@Test
+	fun applicationContextTest() {
+
+	}
+}
